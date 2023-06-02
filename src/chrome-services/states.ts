b94254e19153098c0a94a881f29e8fb7487c8c31
jpoly <jpoly@umich.edu>
2023-06-02 14:41:29 -0400

    #26. fixing dependency tree after refactoring

--- src/chrome-services/states.ts
+++ src/chrome-services/states.ts
@@ -1,3 +1,29 @@
+import { TimeSegDatum, IvqDatum } from "../types";
+
+let timeSegData: TimeSegDatum[] = [];
+const setTimeSegData = (value: TimeSegDatum[]) => {
+  timeSegData = value;
+};
+const pushTimeSegData = (value: TimeSegDatum) => {
+  timeSegData.push(value);
+};
+
+let fullTranscript: string[] = [];
+const setFullTranscript = (value: string[]) => {
+  fullTranscript = value;
+};
+const pushFullTranscript = (value: string) => {
+  fullTranscript.push(value);
+};
+
+let ivqData: IvqDatum[] = [];
+const setIvqData = (value: IvqDatum[]) => {
+  ivqData = value;
+};
+const pushIvqData = (value: IvqDatum) => {
+  ivqData.push(value);
+};
+
 let prevTimestamp = "";
 const setPrevTimestamp = (value: string) => {
   prevTimestamp = value;
@@ -8,5 +34,8 @@ const setHasSeeked = (value: boolean) => {
   hasSeeked = value;
 };
 
+export { timeSegData, setTimeSegData, pushTimeSegData };
+export { fullTranscript, setFullTranscript, pushFullTranscript };
+export { ivqData, setIvqData, pushIvqData };
 export { prevTimestamp, setPrevTimestamp };
 export { hasSeeked, setHasSeeked };
