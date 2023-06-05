cbb4441348169e1055c6ea377c12c637056fcf5e
jpoly <jpoly@umich.edu>
2023-06-05 11:38:11 -0400

    chunk transcript feature

--- src/chrome-services/features/index.ts
+++ src/chrome-services/features/index.ts
@@ -2,4 +2,5 @@ export * from "./transcriptDetectionFeature";
 export * from "./videoDetectionFeature";
 export * from "./ivqDetectionFeature";
 export * from "./copyPasteDetectionFeature";
+export * from "./chunkTranscriptFeature";
 // export * from "./lmMappingFeature";
