f98c187dc1b13c8e10e2b273908dc7d9f505b4a9
jpoly <jpoly@umich.edu>
2023-06-06 15:01:25 -0400

    dropdown menu component that holds LMs

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,30 @@
+import { DropdownItem } from "./DropdownItem";
+import { Lm } from "../types";
+
+interface Props {
+  lmArray: Lm[];
+  handleIndex: (value: number) => void;
+}
+
+// this is a dropdown menu that lists all the captured learning moments.
+const Dropdown = ({ lmArray, handleIndex }: Props) => {
+  const handleChange = () => {
+    const e = document.getElementById("lmSelectMenu") as HTMLSelectElement;
+    const value = +e.value;
+    // const text = e.options[e.selectedIndex].text;
+    handleIndex(value);
+  };
+
+  return (
+    <>
+      <select name="" id="lmSelectMenu" onChange={handleChange}>
+        <option value="-1">Select a learning moment...</option>
+        {lmArray.map((lm, index) => {
+          return <DropdownItem key={index} index={index} value={lm.start_time} />;
+        })}
+      </select>
+    </>
+  );
+};
+
+export { Dropdown };
