0da240d2459dd01529674158d8eaaafc91c5e743
jpoly <jpoly@umich.edu>
2023-06-07 13:51:35 -0400

    support highlighting based on correctness

--- src/components/Mcq.tsx
+++ src/components/Mcq.tsx
@@ -1,27 +1,38 @@
 import { QuestionField } from "./QuestionField";
 import { AnswerField } from "./AnswerField";
-import { Lm } from "../types";
+import { Lm, AnswerChoice } from "../types";
 
 interface Props {
   lmArray: Lm[];
   index: number;
+  handleClick: (i: number, acIndex: number) => void;
 }
 
-const Mcq = ({ lmArray, index }: Props) => {
+const Mcq = ({ lmArray, index, handleClick }: Props) => {
+  console.log("MCQ loaded!");
   let question = "";
-  let answers: string[] = [];
+  let answerChoices: AnswerChoice[] = [];
 
   if (index >= 0) {
     question = lmArray[index].md_flashcard[0].question;
-    answers = lmArray[index].md_flashcard[0].answers;
+    answerChoices = lmArray[index].md_flashcard[0].answerChoices;
   }
+
   return (
     <>
       <h3>MCQ:</h3>
       {index >= 0 && <QuestionField question={question} />}
       {index >= 0 &&
-        answers.map((answer, answerIndex) => {
-          return <AnswerField key={answerIndex} answerIndex={answerIndex} answer={answer} />;
+        answerChoices.map((answerChoice, answerChoiceIndex) => {
+          return (
+            <AnswerField
+              key={answerChoiceIndex}
+              answerIndex={index}
+              answerChoiceIndex={answerChoiceIndex}
+              answerChoice={answerChoice}
+              handleClick={handleClick}
+            />
+          );
         })}
     </>
   );
