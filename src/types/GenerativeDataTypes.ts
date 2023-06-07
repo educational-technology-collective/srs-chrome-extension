abb90fe4986a12a0017da446dc3fcf986618e02e
jpoly <jpoly@umich.edu>
2023-06-07 13:50:33 -0400

    fixing data types. AnswerChoice.color is green if correct, and red if incorrect.

--- src/types/GenerativeDataTypes.ts
+++ src/types/GenerativeDataTypes.ts
@@ -1,12 +1,20 @@
+export interface AnswerChoice {
+  answer: string;
+  color: string;
+  isClicked: boolean;
+}
+
 export interface Flashcard {
   question: string;
-  answers: string[];
+  answerChoices: AnswerChoice[];
 }
 
 export interface Lm {
+  id: number;
   start_time: string;
   end_time: string;
   concept_name: string;
   concept_classification: string;
   md_flashcard: Flashcard[];
+  deleted: boolean;
 }
