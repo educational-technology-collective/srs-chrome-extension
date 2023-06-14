a630ccde3b14b2a4722823f1c95eb8729b1ed0dc
jpoly <jpoly@umich.edu>
2023-06-14 11:17:42 -0400

    #33. handles auth flow with Google sign in

--- src/chrome-services/serviceWorker.ts
+++ src/chrome-services/serviceWorker.ts
@@ -19,3 +19,28 @@ chrome.action.onClicked.addListener((tab) => {
     }
   })();
 });
+
+let user_signed_in = false;
+chrome.identity.onSignInChanged.addListener(function (account_id, signedIn) {
+  if (signedIn) {
+    user_signed_in = true;
+    console.log("user signed in:", user_signed_in);
+    console.log("account id:", account_id);
+  } else {
+    user_signed_in = false;
+  }
+});
+
+chrome.runtime.onMessage.addListener((request) => {
+  if (request.message === "get_auth_token") {
+    chrome.identity.getAuthToken({ interactive: true }, function (token) {
+      console.log("token:", token);
+    });
+  } else if (request.message === "get_profile") {
+    chrome.identity.getProfileUserInfo({ accountStatus: "ANY" }, function (user_info) {
+      console.log("user info", user_info);
+    });
+  }
+
+  return true;
+});
