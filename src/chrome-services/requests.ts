afcb8b35ba35e33e2e661e7cc8bfbb5559d9f786
jpoly <jpoly@umich.edu>
2023-06-19 16:25:34 -0400

    get request

--- src/chrome-services/requests.ts
+++ src/chrome-services/requests.ts
@@ -19,3 +19,32 @@ export const makePostReq = async (endpoint: string, payload: object) => {
     console.log(error);
   }
 };
+
+// makes GET request to the given endpoint of the AWS Lambda instance.
+// endpoint should lead with a slash.
+// params is an array of param-paramValue pair.
+// for example, if my URL parameter is ?param1=pv1&param2=pv2,
+// params = [["param1", "pv1"], ["param2", "pv2"]]
+export const makeGetReq = async (endpoint: string, params: string[][]) => {
+  try {
+    let paramStr = "?";
+    params.forEach((param) => {
+      const p = `${param[0]}=${encodeURIComponent(param[1])}&`;
+      paramStr += p;
+    });
+    paramStr = paramStr.substring(0, paramStr.length - 1);
+    console.log(paramStr);
+    const url = API_GATEWAY + endpoint + paramStr;
+    const resp = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    const data = await resp.json();
+    console.log("GET:", data);
+  } catch (error) {
+    console.log(error);
+  }
+};
