3bb5129465476b8da5b5bc559046aa06979dfc2f
jpoly <jpoly@umich.edu>
2023-06-22 15:25:38 -0400

    #42. Concepts can be added and deleted

--- src/components/ConceptLabel.tsx
+++ src/components/ConceptLabel.tsx
@@ -1,18 +1,40 @@
+import { Concept, VideoLm } from "../types";
 import "../styles/ConceptLabel.css";
 
 interface Props {
-  concept: string;
+  concepts: Concept[];
+  conceptIndex: number;
+  setConcepts: React.Dispatch<React.SetStateAction<Concept[]>>;
+  lmArray: VideoLm[];
+  lmIndex: number;
+  updateArr: (value: VideoLm[]) => void;
 }
 
-const ConceptLabel = ({ concept }: Props) => {
+const ConceptLabel = ({ concepts, conceptIndex, setConcepts, lmArray, lmIndex, updateArr }: Props) => {
+  const handleDelete = () => {
+    const newConcepts: Concept[] = JSON.parse(JSON.stringify(concepts));
+    newConcepts.splice(conceptIndex, 1);
+    console.log("deleted:", newConcepts);
+
+    setConcepts(newConcepts);
+
+    const newLmArray: VideoLm[] = JSON.parse(JSON.stringify(lmArray));
+    newLmArray[lmIndex].concepts = newConcepts;
+    updateArr(newLmArray);
+
+    // TODO: DELETE request to DB.
+  };
+
   return (
     <>
       <div className="conceptContainer">
         <div className="concept">
-          <span>{concept}</span>
+          <span>{concepts[conceptIndex].name}</span>
         </div>
         <div className="delConceptBtnContainer">
-          <button className="delConceptBtn">X</button>
+          <button className="delConceptBtn" onClick={handleDelete}>
+            X
+          </button>
         </div>
       </div>
     </>
--- src/components/Pane2.tsx
+++ src/components/Pane2.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useEffect, useState } from "react";
 import { ConceptLabel } from ".";
 import { VideoLm, Concept } from "../types";
 import "../styles/Pane2.css";
@@ -5,24 +6,92 @@ import "../styles/Pane2.css";
 interface Props {
   lmArray: VideoLm[];
   index: number;
+  updateArr: (value: VideoLm[]) => void;
 }
 
-const Pane2 = ({ lmArray, index }: Props) => {
-  let concepts: Concept[] = [];
-  if (index >= 0) {
-    concepts = lmArray[index].concepts;
-  }
+const Pane2 = ({ lmArray, index, updateArr }: Props) => {
+  const [concepts, setConcepts] = useState([] as Concept[]);
+  useEffect(() => {
+    if (index >= 0) {
+      setConcepts(lmArray[index].concepts);
+    }
+  }, [index, lmArray]);
+
+  const [mode, setMode] = useState("display");
+
+  const handleClick = () => {
+    if (mode === "display") {
+      setMode("add");
+    } else {
+      setMode("display");
+    }
+  };
+
+  const [newConcept, setNewConcept] = useState("");
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    const newConcepts = JSON.parse(JSON.stringify(concepts));
+    newConcepts.push({ id: "", name: newConcept } as Concept);
+    setConcepts(newConcepts);
+
+    const newLmArray: VideoLm[] = JSON.parse(JSON.stringify(lmArray));
+    newLmArray[index].concepts = newConcepts;
+    updateArr(newLmArray);
+
+    setMode("display");
+
+    // TODO: post to DB
+  };
 
   return (
     <>
       <div id="pane2Container">
-        <p id="conceptHeader">Concepts:</p>
-        {index >= 0 &&
-          concepts &&
-          concepts.map((concept, idx) => {
-            return <ConceptLabel key={idx} concept={concept.name} />;
-          })}
-        <button className="addConceptBtn">+</button>
+        <div id="conceptHeaderContainer">
+          <p id="conceptHeader">Concepts:</p>
+        </div>
+        {mode === "display" && (
+          <>
+            <div id="conceptLabelContainer">
+              {index >= 0 &&
+                concepts &&
+                concepts.map((_concept, idx) => {
+                  return (
+                    <ConceptLabel
+                      key={idx}
+                      concepts={concepts}
+                      conceptIndex={idx}
+                      setConcepts={setConcepts}
+                      lmArray={lmArray}
+                      lmIndex={index}
+                      updateArr={updateArr}
+                    />
+                  );
+                })}
+            </div>
+          </>
+        )}
+        {mode === "add" && (
+          <>
+            <div id="addFormContainer">
+              <form id="addForm" onSubmit={handleSubmit}>
+                <textarea
+                  id="conceptInput"
+                  rows={5}
+                  value={newConcept}
+                  onChange={(e) => setNewConcept(e.target.value)}
+                />
+                <button type="submit">Add new concept</button>
+              </form>
+            </div>
+          </>
+        )}
+        <div id="btnContainer">
+          <button id="addConceptBtn" onClick={handleClick}>
+            {mode === "display" ? "Add" : "Cancel"}
+          </button>
+        </div>
       </div>
     </>
   );
