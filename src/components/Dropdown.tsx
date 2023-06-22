5e9264aa160eb10102830ab230cbcbce52255205
jpoly <jpoly@umich.edu>
2023-06-22 15:23:15 -0400

    dropdown index bug fix

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -5,10 +5,11 @@ import "../styles/Dropdown.css";
 interface Props {
   lmArray: VideoLm[];
   handleIndex: (value: number) => void;
+  index: number;
 }
 
 // this is a dropdown menu that lists all the captured learning moments.
-const Dropdown = ({ lmArray, handleIndex }: Props) => {
+const Dropdown = ({ lmArray, handleIndex, index }: Props) => {
   const handleChange = () => {
     const e = document.getElementById("lmSelectMenu") as HTMLSelectElement;
     const value = +e.value;
@@ -21,9 +22,9 @@ const Dropdown = ({ lmArray, handleIndex }: Props) => {
       <div id="dropdown">
         <select name="" id="lmSelectMenu" onChange={handleChange}>
           <option value="-1">Select a learning moment...</option>
-          {lmArray.map((lm, index) => {
+          {lmArray.map((lm, idx) => {
             // return !lm.deleted && <DropdownItem key={index} index={index} value={lm.endTime} />;
-            return <DropdownItem key={index} index={index} value={lm.endTime} />;
+            return <DropdownItem key={idx} index={idx} value={lm.endTime} />;
           })}
         </select>
       </div>
