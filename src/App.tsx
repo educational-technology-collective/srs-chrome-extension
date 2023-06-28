854f0c034ed8296cb090bf23a82b411f5f97c5cb
jpoly <jpoly@umich.edu>
2023-06-28 03:26:56 -0400

    deleted unnecessary comments

--- src/App.tsx
+++ src/App.tsx
@@ -28,20 +28,6 @@ function App() {
       .catch((err) => {
         console.log("Error while fetching videoLM:", err);
       });
-
-    // prod:
-    // const b64url = btoa(window.location.toString());
-    // const b64url = btoa("https://www.coursera.org/learn/python-data-analysis/lecture/Kgwr5/merging-dataframes");
-    // makeGetReq(`/lms/videoId/${b64url}`)
-    //   .then((res) => {
-    //     setArr(res);
-    //     setIndex(0);
-    //     // send message to the service worker, so that it can update the state in chrome-services directory.
-    //     chrome.runtime.sendMessage({ message: "GET from App", data: res });
-    //   })
-    //   .catch((err) => {
-    //     console.log("Error while fetching videoLM:", err);
-    //   });
   }, []);
 
   // lmArray index handling.
@@ -51,62 +37,6 @@ function App() {
     setIndex(value);
   };
 
-  // updates the lmArray itself.
-  // used to delete elements from the array.
-  // const deleteArr = (i: number) => {
-  //   if (i === -1) {
-  //     return;
-  //   }
-
-  //   const newLmArray = arr.map((lm, idx) => {
-  //     if (i === idx) {
-  //       lm.deleted = true;
-  //       return lm;
-  //     } else {
-  //       return lm;
-  //     }
-  //   });
-
-  //   setArr(newLmArray);
-  //   setIndex(-1);
-  //   alert("deleted!");
-  // };
-
-  // save elements.
-  // const saveArr = (i: number) => {
-  //   if (i === -1) {
-  //     return;
-  //   }
-
-  //   const newLmArray = arr.map((lm, idx) => {
-  //     if (i === idx) {
-  //       lm.saved = true;
-  //       return lm;
-  //     } else {
-  //       return lm;
-  //     }
-  //   });
-
-  //   setArr(newLmArray);
-  //   console.log(newLmArray);
-  //   alert("saved!");
-  // };
-
-  // determines whether an answer choice is clicked or not.
-  // makes sure that the choice turns green or red, or transparent when reset.
-  // const handleClick = (i: number, acIndex: number) => {
-  //   const newLmArray = arr.map((lm, idx) => {
-  //     if (i === idx) {
-  //       lm.mdFlashcard[0].answerChoices[acIndex].isClicked = true;
-  //       return lm;
-  //     } else {
-  //       return lm;
-  //     }
-  //   });
-
-  //   setArr(newLmArray);
-  // };
-
   return (
     <>
       <div id="pane1">
@@ -115,7 +45,6 @@ function App() {
       <div id="pane2">
         <Pane2 lmArray={arr} index={index} updateArr={updateArr} />
       </div>
-      {/* <Mcq lmArray={arr} index={index} handleClick={handleClick} /> */}
       <div id="pane3">
         <Pane3 lmArray={arr} lmIndex={index} updateArr={updateArr} />
         {/* <DeleteButton name={"Delete this learning moment"} index={index} deleteArr={deleteArr} /> */}
