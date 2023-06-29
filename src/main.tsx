caa66879f105cfa3b77fc5c2d7a5884402aa5519
jpoly <jpoly@umich.edu>
2023-06-29 13:58:43 -0400

    #33. Implementing auth0 and adding a check to display items depending on auth status

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./styles/index.css";
 
 console.log(window.location.origin);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-cra0zttj8xlwi6sh.us.auth0.com"
+    clientId="8bc0qUmibSf3yVXzq0ovLQaDLoSGqZTW"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
